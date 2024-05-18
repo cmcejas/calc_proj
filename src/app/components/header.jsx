82054c63bd024c36499d0242bbfb3ccf16852bfc
Carlos <carlosmcejas@gmail.com>
2024-05-18 17:53:05 -0400

    Update header.jsx

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <section>
             <div className="flex bg-slate-800 border-b-4 border-cyan-500 md:px-4 py-3">
-                <Image height={35} width={35} md:width={50} md:height={50} src={svg} alt="logo" className=" drop-shadow-2xl ml-2 mr-2 md:mr-10"/>
+                <Image width={50} height={50} src={svg} alt="logo" className=" drop-shadow-2xl ml-2 mr-2 md:mr-10"/>
 
                 <div className="text-white md:text-xl font-medium my-auto gap-6 md:gap-16 flex">
                     <Link href="/derivatives" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Derivatives </Link>
