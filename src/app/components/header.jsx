972d84bd728d99a1fe4b776bca5d5625894e2210
cmcejas <carlosmcejas@gmail.com>
2024-05-18 17:43:15 -0400

    new color design

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -6,10 +6,10 @@ import Link from 'next/link'
 const Header = () => {
     return(
         <section>
-            <div className="flex bg-[#D9D9D9] border-b-4 border-black md:px-4 py-3">
-                <Image  width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2 md:mr-10"/>
+            <div className="flex bg-slate-800 border-b-4 border-cyan-500 md:px-4 py-3">
+                <Image  width={50} height={50} src={svg} alt="logo" className=" drop-shadow-2xl ml-2 mr-2 md:mr-10"/>
 
-                <div className=" md:text-xl font-medium my-auto gap-6 md:gap-16 flex">
+                <div className="text-white md:text-xl font-medium my-auto gap-6 md:gap-16 flex">
                     <Link href="/derivatives" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Derivatives </Link>
                     <Link href="/integrals" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Integrals </Link>
                     <Link href="/ap_practice" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">AP Practice </Link>
