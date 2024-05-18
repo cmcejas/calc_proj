4a7593c768b5b5e014303c6e06996c14409e787d
cmcejas <carlosmcejas@gmail.com>
2024-05-18 15:09:37 -0400

    Initiated calc proj

---
+++ src/app/components/header.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import svg from './logo.svg';
+import Link from 'next/link'
+
+
+const Header = () => {
+    return(
+        <section>
+            <div className="flex bg-[#D9D9D9] border-b-4 border-black md:px-4 py-3">
+                <Image  width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2 md:mr-10"/>
+
+                <div className=" md:text-xl font-medium my-auto gap-6 md:gap-16 flex">
+                    <Link href="/derivatives" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Derivatives </Link>
+                    <Link href="/integrals" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Integrals </Link>
+                    <Link href="/ap_practice" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">AP Practice </Link>
+                </div>
+
+            </div>
+
+        </section>
+
+        );
+
+    };
+export default Header;
---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import Header from './components/header';
+import Image from 'next/image';
+import bg from '../../public/bg_final.svg';
+
+export default function Home() {
+  return (
+    <main>
+      <div className="min-h-screen min-w-screen bg-gradient-to-b from-transparent to-white">
+        <Header />
+
+        <Image className="" src={bg} alt="logo"/>
+
+
+      </div>
+    </main>
+      );
+}
