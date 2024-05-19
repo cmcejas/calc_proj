04700614c0feb8de366d657d61adac490f8a06f1
cmcool <75821231+cmcool@users.noreply.github.com>
2024-05-18 23:20:55 -0400

    image resizes nicer

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -6,13 +6,13 @@ import Link from 'next/link'
 const Header = () => {
     return(
         <section>
-            <div className="flex bg-slate-800 border-b-4 border-cyan-500 md:px-4 py-3">
-                <Image width={50} height={50} src={svg} alt="logo" className=" drop-shadow-2xl ml-2 mr-2 md:mr-10"/>
+            <div className="flex py-3 border-b-4 bg-slate-800 border-cyan-500 md:px-4">
+                <Image width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10"/>
 
-                <div className="text-white md:text-xl font-medium my-auto gap-6 md:gap-16 flex">
-                    <Link href="/derivatives" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Derivatives </Link>
-                    <Link href="/integrals" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">Integrals </Link>
-                    <Link href="/ap_practice" className="hover:rotate-6 hover:text-3xl duration-200 hover:cursor-pointer">AP Practice </Link>
+                <div className="flex gap-6 my-auto font-medium text-white md:text-xl md:gap-16">
+                    <Link href="/derivatives" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">Derivatives </Link>
+                    <Link href="/integrals" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">Integrals </Link>
+                    <Link href="/ap_practice" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">AP Practice </Link>
                 </div>
 
             </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,14 +4,17 @@ import bg from '../../public/bg_final.svg';
 
 export default function Home() {
   return (
-    <main>
-      <div className="min-h-screen min-w-screen bg-gradient-to-b from-transparent to-white">
-        <Header />
-
-        <Image priority className="" src={bg} alt="landing"/>
-
-
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="relative flex-grow">
+        <Image
+          priority
+          layout="lg:fill"
+          objectFit="cover"
+          src={bg}
+          alt="landing"
+        />
       </div>
     </main>
-      );
+  );
 }
