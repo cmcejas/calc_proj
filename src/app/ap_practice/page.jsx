7ea635f0101b07565a4bfaf95a57a378366744ee
cmcejas <carlosmcejas@gmail.com>
2024-05-21 00:35:44 -0400

    configured other pages

---
+++ src/app/ap_practice/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header';
+
+
+const Ap_practice = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Ap_practice;
--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <section>
             <div className="flex py-3 border-b-4 bg-slate-800 border-cyan-500 md:px-4">
-                <Image width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10"/>
+                <Link href="/"><Image href="/" width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10"/></Link>
 
                 <div className="flex gap-6 my-auto font-medium text-white md:text-xl md:gap-16">
                     <Link href="/derivatives" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">Derivatives </Link>
