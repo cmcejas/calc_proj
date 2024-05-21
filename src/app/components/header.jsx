c292f3b4e930b4eca89258e1990632b73e112182
cmcejas <carlosmcejas@gmail.com>
2024-05-21 01:21:14 -0400

    derivatives page now has some info

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
     return(
         <section>
             <div className="flex py-3 border-b-4 bg-slate-800 border-cyan-500 md:px-4">
-                <Link href="/"><Image href="/" width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10"/></Link>
+                <Link href="/"><Image href="/" width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10 hover:scale-110 hover:-rotate-12 ease-in-out transition duration-100"/></Link>
 
                 <div className="flex gap-6 my-auto font-medium text-white md:text-xl md:gap-16">
-                    <Link href="/derivatives" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">Derivatives </Link>
-                    <Link href="/integrals" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">Integrals </Link>
-                    <Link href="/ap_practice" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer">AP Practice </Link>
+                    <Link href="/derivatives" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">Derivatives </Link>
+                    <Link href="/integrals" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">Integrals </Link>
+                    <Link href="/ap_practice" className="duration-200 hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">AP Practice </Link>
                 </div>
 
             </div>
--- src/app/derivatives/page.jsx
+++ src/app/derivatives/page.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
 import Header from '../components/header';
+import tan from '../../../public/tan.svg';
 
 
 const Derivatives = () => {
   return (
     <div>
       <Header />
+      <div className='md:px-20 px-2 md:flex'>
+        <h1 className="p-4 text-5xl md:text-6xl">Derivative: </h1>
+        <p className="md:pt-7 px-2 text-xl">The derivative of a function describes the function's instantaneous rate of change at a certain point. Another common interpretation is that the derivative gives us the slope of the line tangent to the function's graph at that point.</p>
+      </div>
+      <h1 className="md:mt-12 px-2 md:px-20 p-4 text-5xl md:text-6xl">As a Concept</h1>
+      <div className="lg:flex">
+        <Image className="px-2 md:px-20 rounded-md" src={tan}></Image>
+        <div className='video-wrapper'>
+          <div className="video-responsive">
+            <iframe
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/N2PpRnFqnqY?si=ml9F0W96bXD3HcNm" 
+              title="YouTube video player" 
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin" 
+              allowfullscreen>
+            </iframe>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
