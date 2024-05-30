c5ca08e4e2664d4cabcdd4e5a0cb615360317177
cmcejas <carlosmcejas@gmail.com>
2024-05-30 12:50:20 -0400

    Co-authored-by: HypeEnergy <HypeEnergy@users.noreply.github.com>

--- src/app/integrals/page.jsx
+++ src/app/integrals/page.jsx
@@ -3,7 +3,7 @@ import Header from '../components/header';
 import Image from 'next/image';
 import yxGraph from '../../../public/yxGraph.svg';
 import AreaTriangle from '../../../public/AreaTriangle.webp';
-import antiDeri from '../../../public/AntiDerivatives.png';
+import antiDeri from '../../../public/AntiDerivatives.svg';
 
 import IntegralDesmos from '../components/IntegralDesmos';
 
@@ -21,16 +21,21 @@ const Integrals = () => {
 
       <h1 className="md:mt-12 px-2 md:px-20 p-4 text-5xl md:text-6xl">As a Concept</h1>
 
-      <div className='lg:flex p-1'>
-        <Image className="md:px-15 rounded-lg" alt="y=x" src={yxGraph}></Image>
-        
+      <div className="px-2 md:px-20 p-4">
+        <Image className="md:px-15 rounded-lg" width={500} alt="y=x" src={yxGraph}></Image>
+      </div>
+
+      <div className="text-xs p-4 px-2 md:px-20">
+        <div className="bg-black rounded-3xl pt-0.01 px-2">_</div>
       </div>
 
       <div className='text-center'>
         <h1 className="p-6 text-5xl md:text-6xl">Anti-Derivitaves and Indefinite integrals: </h1>
         
         <p1 className="py-10 text-lg">In order to find the area under a curve, we need to take the anti derivative of the function we are trying to integrate. Here are a few anti derivatives to memorize</p1>
-        <Image className="md:px-15 py-2 inline-block align-middle" alt="antiderivs" src={antiDeri}></Image>
+        <div>
+          <Image width={1200} className="md:px-15 py-2 inline-block align-middle" alt="antiderivs" src={antiDeri}></Image>
+        </div>
       </div>
       
       
