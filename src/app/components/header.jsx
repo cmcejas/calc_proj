c1f0eb5cbc05ea120cca8cc733fc39c935d91240
Carlos <carlosmcejas@gmail.com>
2024-06-02 22:54:49 -0400

    Update header.jsx

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
                 <Link href="/"><Image href="/" width={50} height={50} src={svg} alt="logo" className="ml-2 mr-2  drop-shadow-2xl md:mr-10 hover:scale-110 hover:-rotate-12 ease-in-out transition duration-200"/></Link>
 
                 <div className="flex gap-6 my-auto font-medium text-white md:text-xl md:gap-16">
-                    <Link href="/derivatives" className="duration-200 md:hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">Derivatives </Link>
-                    <Link href="/integrals" className="duration-200 md:hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">Integrals </Link>
-                    <Link href="/ap_practice" className="duration-200 md:hover:rotate-6 hover:text-3xl hover:cursor-pointer ease-in-out">AP Practice </Link>
+                    <Link href="/derivatives" className="duration-200 md:hover:rotate-6 md:hover:text-3xl hover:cursor-pointer ease-in-out">Derivatives </Link>
+                    <Link href="/integrals" className="duration-200 md:hover:rotate-6 md:hover:text-3xl hover:cursor-pointer ease-in-out">Integrals </Link>
+                    <Link href="/ap_practice" className="duration-200 md:hover:rotate-6 md:hover:text-3xl hover:cursor-pointer ease-in-out">AP Practice </Link>
                 </div>
 
             </div>
