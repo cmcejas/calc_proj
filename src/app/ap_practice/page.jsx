663969ad3108c937093568031f348263d4ed78e4
cmcejas <carlosmcejas@gmail.com>
2024-06-03 08:48:05 -0400

    success message done

--- src/app/ap_practice/page.jsx
+++ src/app/ap_practice/page.jsx
@@ -10,6 +10,8 @@ import question1 from '../../../public/ApPracQuestions/question1.webp';
 import question2 from '../../../public/ApPracQuestions/question2.webp';
 import question3 from '../../../public/ApPracQuestions/question3.webp';
 
+const success = "../ApPracQuestions/success.mp4";
+
 const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 const questions = [question1, question2, question3];
@@ -24,6 +26,7 @@ const Ap_practice = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [unansweredQuestions, setUnansweredQuestions] = useState([...questions.keys()]);
+  const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false);
 
   useEffect(() => {
     loadNextQuestion();
@@ -32,6 +35,7 @@ const Ap_practice = () => {
   const loadNextQuestion = () => {
     if (unansweredQuestions.length === 0) {
       toast.success('Congratulations! You have completed all questions.');
+      setAllQuestionsAnswered(true);
       return;
     }
 
@@ -59,26 +63,34 @@ const Ap_practice = () => {
     <div>
       <Header />
       <div className="text-center center">
-        {selectedImage && <img src={selectedImage.src} alt="Random Question" />}
-        <div>
-          <div>
-            {['a', 'b', 'c', 'd', 'e'].map(option => (
-              <label key={option} style={{ marginRight: '10px' }}>
-                <input
-                  type="radio"
-                  value={option}
-                  checked={selectedOption === option}
-                  onChange={(e) => setSelectedOption(e.target.value)}
-                />
-                {option.toUpperCase()}
-              </label>
-            ))}
+        {allQuestionsAnswered ? (
+          <div className="md:mt-20">
+            <ReactPlayer className="" url={success} playing={true} controls={false} />
           </div>
-          <div>
-            <button className="p-2 mt-3 text-sm text-white duration-300 md:hover:-translate-y-0.5 md:hover:bg-cyan-700 rounded-2xl bg-slate-800" onClick={handleAnswerSubmit}>Submit Answer</button>
-          </div>
-        </div>
-        <ToastContainer theme="colored" autoClose={1000}/>
+        ) : (
+          <>
+            {selectedImage && <img src={selectedImage.src} alt="Random Question" />}
+            <div>
+              <div>
+                {['a', 'b', 'c', 'd', 'e'].map(option => (
+                  <label key={option} style={{ marginRight: '10px' }}>
+                    <input
+                      type="radio"
+                      value={option}
+                      checked={selectedOption === option}
+                      onChange={(e) => setSelectedOption(e.target.value)}
+                    />
+                    {option.toUpperCase()}
+                  </label>
+                ))}
+              </div>
+              <div>
+                <button className="p-2 mt-3 text-sm text-white duration-300 md:hover:-translate-y-0.5 md:hover:bg-cyan-700 rounded-2xl bg-slate-800" onClick={handleAnswerSubmit}>Submit Answer</button>
+              </div>
+            </div>
+            <ToastContainer theme="colored" autoClose={1000}/>
+          </>
+        )}
       </div>
     </div>
   );
